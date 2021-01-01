7ae972fb5c8825e39c99b99f9faae3246f8dc353
XiaoHuisir <860663594@qq.com>
2021-01-01 18:16:54 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/yi/mylibrary/SSDemo.java
@@ -0,0 +1,12 @@
+package com.yi.mylibrary;
+
+public class SSDemo {
+    public String getSs(String name){
+        if (name==null||name.equals("")){
+            return "null";
+        }else {
+
+        return name;
+        }
+    }
+}
